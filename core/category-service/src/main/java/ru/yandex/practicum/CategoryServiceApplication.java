762232b449114abb62031c13edd873a12a50d0d5
Semenov Nikita <nik.269@yandex.ru>
2026-04-12 19:20:17 +0300

    Правки

--- core/category-service/src/main/java/ru/yandex/practicum/CategoryServiceApplication.java
+++ core/category-service/src/main/java/ru/yandex/practicum/CategoryServiceApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "ru.yandex.practicum.client")
-@ComponentScan(basePackages = {"ru.yandex.practicum", "ru.yandex.practicum.client"})
 public class CategoryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CategoryServiceApplication.class, args);
--- core/request-service/src/main/java/ru/yandex/practicum/RequestServiceApplication.java
+++ core/request-service/src/main/java/ru/yandex/practicum/RequestServiceApplication.java
@@ -1,22 +1,15 @@
-package ru.yandex.practicum.request;
-
+package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "ru.yandex.practicum.client")
-@ComponentScan(basePackages = {
-        "ru.yandex.practicum.request",
-        "ru.yandex.practicum.client",
-        "ru.yandex.practicum.exception"
-})
 public class RequestServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ru.yandex.practicum.request.RequestServiceApplication.class, args);
+        SpringApplication.run(RequestServiceApplication.class, args);
     }
 }
