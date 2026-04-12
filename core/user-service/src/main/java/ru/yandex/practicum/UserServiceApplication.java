77ce9d27b9da85446b17de0719f3f918e9513267
Semenov Nikita <nik.269@yandex.ru>
2026-04-12 18:22:54 +0300

    Правки

--- core/user-service/src/main/java/ru/yandex/practicum/UserServiceApplication.java
+++ core/user-service/src/main/java/ru/yandex/practicum/UserServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = {"ru.yandex.practicum.user", "ru.yandex.practicum.exception"})
+@ComponentScan(basePackages = {"ru.yandex.practicum.exception"})
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
