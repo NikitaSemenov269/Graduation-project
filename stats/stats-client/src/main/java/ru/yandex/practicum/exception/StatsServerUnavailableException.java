168ba562823d6a40cad3cafcc418a31b0e6d594d
Semenov Nikita <nik.269@yandex.ru>
2026-04-12 17:33:49 +0300

    Правки

--- stats/stats-client/src/main/java/ru/yandex/practicum/exception/StatsServerUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.exception;
-
-public class StatsServerUnavailableException extends RuntimeException {
-    public StatsServerUnavailableException(String message) {
-        super(message);
-    }
-}
